16056504ef9c3362dbb8cea42bdaecc6920b7e65
Pragalvha Sharma <140682795+PragalvhaSharma@users.noreply.github.com>
2024-02-27 11:19:27 -0500

    Update LinkedNumberException.java

--- LinkedNimber/LinkedNumberException.java
+++ LinkedNimber/LinkedNumberException.java
@@ -1,6 +1,9 @@
+//Custom exception for an invalid condition or operation in LinkedNumber objects.
+// Inherits from RuntimeException class
 public class LinkedNumberException extends RuntimeException {
 
+    //Constructs an exception with a message.
     public LinkedNumberException(String msg) {
-        super(msg);
+        super(msg); // Initialize RuntimeException with the given message.
     }
 }
