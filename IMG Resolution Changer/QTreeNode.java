03590551e327753339910b92738d61c7562db5d5
Pragalvha Sharma <140682795+PragalvhaSharma@users.noreply.github.com>
2024-04-05 14:16:08 -0400

    Update QTreeNode.java

--- IMG Resolution Changer/QTreeNode.java
+++ IMG Resolution Changer/QTreeNode.java
@@ -1,5 +1,8 @@
 
+// QTreeNode represents a node in a QuadrantTree data structure
 public class QTreeNode {
+	
+	//Instance Variables
 	private int x;
 	private int y;
 	private int size;
@@ -7,6 +10,7 @@ public class QTreeNode {
 	private QTreeNode parent;
 	private QTreeNode[] children; 
 	
+	//Constructor without parameter with default values and no children
 	public QTreeNode() {
 		parent = null;
 		children = new QTreeNode[4];
@@ -16,6 +20,7 @@ public class QTreeNode {
 		color=0;
 	}
 	
+	//Constructor with parameters parameters with specified characteristics and children
 	public QTreeNode(QTreeNode[] theChildren, int xcoord, int ycoord, int theSize, int theColor) {
 		parent = null;
 		children = theChildren;
@@ -25,6 +30,7 @@ public class QTreeNode {
 		color = theColor;
 	}
 	
+	// Checks if this node contains a point
 	public boolean contains(int xcoord, int ycoord) {
 		//Checking x
 		if((xcoord  >= x && xcoord <= (x + size -1)) && (ycoord >= y && ycoord <= (y+size-1)) ) {
@@ -34,65 +40,80 @@ public class QTreeNode {
 	
 		}
 	
+	// Getter for the x-coordinate
 	public int getx() {
 		return x;
 	}
 	
+	// Getter for the y-coordinate
 	public int gety() {
 		return y;
 	}
 	
+	//Getter for the size
 	public int getSize() {
 		return size;
 	}
 	
+	// Getter for the color
 	public int getColor() {
 		return color;
 	}
 	
+	// Getter for the parent node
 	public QTreeNode getParent() {
 		return parent;
 	}
 	
+	// Retrieves a child node at the specified index
 	public QTreeNode getChild(int index) throws QTreeException {
 		if (children == null || index < 0 || index > 3) {
+			//if index is out of bounds
 			throw new QTreeException("Error for index: " + index);
 			}
 		return children[index];
 	}
 	
+	// Setter for x
 	public void setx(int newx) {
 		x = newx;
 	}
 	
+	// Setter for y
 	public void sety(int newy) {
 		y = newy;
 	}
 	
+	// Setter for size
 	public void setSize(int newSize) {
 		size = newSize;
 	}
 	
+	// Setter for color
 	public void setColor(int newColor) {
 		color = newColor;
 	}
 	
+	// Setter for parent
 	public void setParent(QTreeNode newParent) {
 		parent = newParent;
 	}
 	
+	// Setter for child
 	public void setChild (QTreeNode newChild, int index) throws QTreeException {
+		// Check if index is out of bounds
 		if (children == null || index < 0 || index > 3) {
 			throw new QTreeException("Error for index: " + index);
 			}
 		children[index] = newChild;
 	}
 	
+	// Checks if this node is a leaf node
 	public boolean isLeaf() {
 		if(children == null) {
 			return true;
 		}
-		
+		//Check if every entry of children is null
 		for (int i = 0; i < children.length; i++) {
 			if(children[i] != null) {
 				return false;
@@ -101,5 +122,7 @@ public class QTreeNode {
 		return true;
 	}
 }
+
+
 
 
